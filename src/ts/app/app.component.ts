4f4266183999e04883aeb1205cebc4d014884849
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-10-06 13:15:09 -0300

    Creating a Custom Telephone Validator Function

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+
+const phoneNumberValidator = (control: FormControl) => {
+
+    if (control.value == null || String(control.value).length === 0) {
+        return null;
+    }
+
+    const re = new RegExp("^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$");
+
+    if (!re.test(control.value)) {
+        return {
+            phoneNumber: true,
+        };
+    }
+
+    return null;
+}
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    template: `
+        <div>
+            <label for="phone-number-input">Phone Number:</label>
+            <input type="tel" id="phone-number-input" [formControl]="phoneNumberInput">
+            <span *ngIf="phoneNumberInput.errors?.phoneNumber">
+                Phone is not valid.
+            </span>
+            <span *ngIf="phoneNumberInput.errors?.required">
+                Phone is not required.
+            </span>
+        </div>
+    `,
 })
 export class AppComponent {
 
-    public message: string = "Hello World!";
+    public phoneNumberInput = new FormControl("", [phoneNumberValidator, Validators.required]);
 
 }
